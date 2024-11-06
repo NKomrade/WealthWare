aa4ccbc7c00cc52909dab9e46d1eaea52c15669d
Nitin Kumar Singh <dear.nk.comrade@gmail.com>
2024-11-06 15:55:17 +0530

    2

--- src/pages/Sales_Report.jsx
+++ src/pages/Sales_Report.jsx
@@ -135,7 +135,7 @@ function SalesReport() {
     }
 
     return (
-        <div className="p-6 bg-gray-100 flex space-x-6">
+        <div className="p-2 bg-gray-100 flex space-x-6">
             <div className="flex-1 bg-white shadow-md rounded-md p-4 space-y-6">
                     <h1 className="text-2xl font-bold mb-4 text-center">
                         Sales Report from{' '}
